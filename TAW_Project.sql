f1fec4e1770efbbfda4835abb71c447b8ed691b7
alejandroat99 <alejandroat99@uma.es>
2020-03-25 13:51:40 +0100

    Añadidas claves con autoincremento y arregladas algunas FK.

--- TAW_Project.sql
+++ TAW_Project.sql
@@ -2,19 +2,19 @@
 -- DROP TABLES --
 -----------------
 
+DROP TABLE COMENTARIO;
+DROP TABLE PRODUCTO_PALABRA_CLAVE;
+DROP TABLE PRODUCTO;
 DROP TABLE USUARIO;
 DROP TABLE CATEGORIA;
 DROP TABLE PALABRA_CLAVE;
-DROP TABLE PRODUCTO;
-DROP TABLE COMENTARIO;
-DROP TABLE PRODUCTO_PALABRA_CLAVE;
 
 ----------------------------
 -- CREACION DE LAS TABLAS --
 ----------------------------
 
 CREATE TABLE USUARIO (
-    id_usuario INTEGER , /* PK */
+    id_usuario INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), /* PK */
     administrador BOOLEAN,
     username VARCHAR(50),
     password VARCHAR(50),
@@ -24,7 +24,7 @@ CREATE TABLE USUARIO (
 );
 
 CREATE TABLE CATEGORIA (
-    id_categoria INTEGER, /* PK */
+    id_categoria INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), /* PK */
     nombre VARCHAR(50),
     supercategoria INTEGER, /* FK -> CATEGORIA  */
     PRIMARY KEY (id_categoria),
@@ -33,20 +33,20 @@ CREATE TABLE CATEGORIA (
 
 
 CREATE TABLE PALABRA_CLAVE (
-    id_palabra_clave INTEGER, /* PK */
+    id_palabra_clave INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), /* PK */
     palabra VARCHAR(50),
     PRIMARY KEY (id_palabra_clave)
 );
 
 CREATE TABLE PRODUCTO(
-    id_producto INTEGER, /* PK */
+    id_producto INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), /* PK */
     nombre VARCHAR(50),
     descripcion VARCHAR(200),
     precio DECIMAL(10,2),
     fecha DATE,
     imagen VARCHAR(100),
-    propietario INTEGER, /* FK -> USUARIO */
-    categoria INTEGER, /* FK -> CATEGORIA */
+    propietario INTEGER NOT NULL, /* FK -> USUARIO */
+    categoria INTEGER NOT NULL, /* FK -> CATEGORIA */
     PRIMARY KEY (id_producto),
     CONSTRAINT usuario_producto_fk FOREIGN KEY (propietario) REFERENCES USUARIO(id_usuario),
     CONSTRAINT categoria_producto_fk FOREIGN KEY (categoria) REFERENCES CATEGORIA(id_categoria)
@@ -56,16 +56,16 @@ CREATE TABLE COMENTARIO(
     texto VARCHAR(200),
     valoracion INTEGER,
     fecha DATE,
-    autor INTEGER, /* FK -> USUARIO */ /* PK */
-    producto INTEGER, /* FK -> PRODUCTO */ /* PK */
+    autor INTEGER NOT NULL, /* FK -> USUARIO */ /* PK */
+    producto INTEGER NOT NULL, /* FK -> PRODUCTO */ /* PK */
     PRIMARY KEY (autor, producto),
     CONSTRAINT usuario_comentario_fk FOREIGN KEY (autor) REFERENCES USUARIO(id_usuario),
     CONSTRAINT producto_comentario_fk FOREIGN KEY (producto) REFERENCES PRODUCTO(id_producto)
 );
 
 CREATE TABLE PRODUCTO_PALABRA_CLAVE(
-    producto INTEGER, /* FK -> PRODUCTO */ /* PK */
-    palabra_clave INTEGER, /* FK -> PALABRA_CLAVE */ /* PK */
+    producto INTEGER NOT NULL, /* FK -> PRODUCTO */ /* PK */
+    palabra_clave INTEGER NOT NULL, /* FK -> PALABRA_CLAVE */ /* PK */
     PRIMARY KEY (producto, palabra_clave),
     CONSTRAINT producto_palabra_clave_fk FOREIGN KEY (producto) REFERENCES PRODUCTO(id_producto),
     CONSTRAINT palabra_clave_producto_fk FOREIGN KEY (palabra_clave) REFERENCES PALABRA_CLAVE(id_palabra_clave)
