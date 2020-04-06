06d641b17e6c96b2d941e84d1ba8e42e19660ec4
Marco <48439949+MarcohbUma@users.noreply.github.com>
2020-04-06 12:43:23 +0200

    creado comentarioDTO

---
+++ Mercafour/src/java/mercafour/dto/ComentarioDTO.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mercafour.dto;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Marco Hurtado
+ */
+public class ComentarioDTO {
+    private String texto;
+    private Integer valoracion;
+    private Date fecha;
+    private UsuarioDTO autor;
+    private ProductoDTO producto;
+    
+    public ComentarioDTO(){}
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Integer getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(Integer valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public UsuarioDTO getAutor() {
+        return autor;
+    }
+
+    public void setAutor(UsuarioDTO autor) {
+        this.autor = autor;
+    }
+
+    public ProductoDTO getProducto() {
+        return producto;
+    }
+
+    public void setProducto(ProductoDTO producto) {
+        this.producto = producto;
+    }
+}
