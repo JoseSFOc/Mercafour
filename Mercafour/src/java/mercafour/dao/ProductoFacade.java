d54c078bf62d9b18bc7afb4f733f0500cad88f96
Marco <48439949+MarcohbUma@users.noreply.github.com>
2020-04-06 12:22:11 +0200

    Añadido métodos para buscar por varios parámetros. Aún faltan...

--- Mercafour/src/java/mercafour/dao/ProductoFacade.java
+++ Mercafour/src/java/mercafour/dao/ProductoFacade.java
@@ -5,9 +5,17 @@
  */
 package mercafour.dao;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import mercafour.entity.Categoria;
 import mercafour.entity.Producto;
 
 /**
@@ -29,4 +37,64 @@ public class ProductoFacade extends AbstractFacade<Producto> {
         super(Producto.class);
     }
     
+    public List<Producto> findByDate (String fecha) {
+        Query q;
+        
+        Date date;
+        try {
+            date = DateFormat.getDateInstance().parse(fecha);
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+        
+        q = this.getEntityManager().createQuery("SELECT p FROM Producto p WHERE p.fecha = :fecha");
+        q.setParameter("fecha", fecha);
+        return q.getResultList();        
+    } 
+    
+    public List<Producto> findByDateDesc () {
+        Query q;
+        
+        q = this.getEntityManager().createQuery("SELECT p FROM Producto p ORDER BY p.fecha DESC");
+        return q.getResultList();        
+    }  
+
+    public List<Producto> findByName (String nombre) {
+        Query q;
+        
+        q = this.getEntityManager().createQuery("SELECT p FROM Producto p WHERE p.nombre LIKE :nombre");
+        q.setParameter("nombre", "%" + nombre + "%"); // Los % se ponen delante y detrás para indicar que la cadena
+                                                      // de caracteres debe estar incluida en el nombre.
+        return q.getResultList();        
+    }
+    
+    public List<Producto> findByCategory (String categoria) {
+        Query q;
+        
+        q = this.getEntityManager().createQuery("SELECT p FROM Producto p WHERE p.categoria LIKE :categoria");
+        q.setParameter("categoria", "%" + categoria + "%"); // Los % se ponen delante y detrás para indicar que la cadena
+                                                      // de caracteres debe estar incluida en el nombre.
+        return q.getResultList();        
+    }
+    
+    /*
+    public List<Producto> findByKeyWord(String categoria) {
+        Query q;
+        
+        q = this.getEntityManager().createQuery("SELECT p FROM Producto p WHERE p.categoria LIKE :categoria");
+        q.setParameter("categoria", "%" + categoria + "%"); // Los % se ponen delante y detrás para indicar que la cadena
+                                                      // de caracteres debe estar incluida en el nombre.
+        return q.getResultList();        
+    }        
+    
+    public List<Customer> findByMicroMarketAndName (String codigopostal, String nombre) {
+        Query q;
+        
+        q = this.getEntityManager().createQuery("SELECT c FROM Customer c WHERE c.zip.zipCode = :codigo AND c.name LIKE :nombre");
+        q.setParameter("codigo", codigopostal);
+        q.setParameter("nombre", "%" + nombre + "%"); // Los % se ponen delante y detrás para indicar que la cadena
+                                                      // de caracteres debe estar incluida en el nombre.
+        return q.getResultList();        
+    }  
+    */
 }
