ab2a490caf50d03ff3314660c13134a377b815df
Marco <48439949+MarcohbUma@users.noreply.github.com>
2020-04-06 20:11:45 +0200

    añadido crear comentario

--- Mercafour/src/java/mercafour/service/ProductosService.java
+++ Mercafour/src/java/mercafour/service/ProductosService.java
@@ -5,14 +5,20 @@
  */
 package mercafour.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import mercafour.dao.ComentarioFacade;
 import mercafour.dao.ProductoFacade;
+import mercafour.dao.UsuarioFacade;
+import mercafour.dto.ComentarioDTO;
 import mercafour.dto.ProductoDTO;
+import mercafour.entity.Comentario;
 import mercafour.entity.Producto;
 import mercafour.entity.Usuario;
 
@@ -27,6 +33,12 @@ public class ProductosService {
 
     @EJB
     private ProductoFacade productoFacade;
+    
+    @EJB
+    private UsuarioFacade usuarioFacade;
+    
+    @EJB
+    private ComentarioFacade comentarioFacade;
 
     protected List<ProductoDTO> convertToDTO(List<Producto> listaProductos) {
         List<ProductoDTO> listaDTO = null;
@@ -67,6 +79,38 @@ public class ProductosService {
         } else {
             this.productoFacade.remove(producto);
             return true;
+        }
+    }
+    
+    public boolean nuevoComentario(String productoId, String texto, String valoracion, String autor, String fecha){
+        Producto producto = this.productoFacade.find(new Integer(productoId));
+        if (producto == null) { //Esta situación no debería darse
+            LOG.log(Level.SEVERE, "No se ha encontrado el producto");
+            return false;
+        } else {
+            if (this.usuarioFacade.findByName(autor) == null) {
+                LOG.log(Level.SEVERE, "No existe el usuario");
+                return false;
+            } else {
+                SimpleDateFormat format = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+                //ComentarioDTO comentario = new ComentarioDTO();
+                Comentario comentario = new Comentario();
+                comentario.setTexto(texto);
+                //comentario.setAutor(this.usuarioFacade.findByName(autor).getDTO());
+                comentario.setUsuario(usuarioFacade.findByName(autor));
+                try {
+                    comentario.setFecha(format.parse(fecha));
+                } catch (ParseException ex) {
+                    LOG.log(Level.SEVERE, "Formato de fecha incorrecto");
+                }
+                //comentario.setProducto(producto.getDTO());
+                comentario.setProducto1(producto);
+                comentario.setValoracion(new Integer(valoracion));
+                
+                this.comentarioFacade.create(comentario);
+                return true;
+            }
+            
         }
     }
 
