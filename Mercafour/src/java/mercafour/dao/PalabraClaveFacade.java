25ae2297e00c597c54d97e4ff667717698abcf24
Marco <48439949+MarcohbUma@users.noreply.github.com>
2020-04-15 17:41:58 +0200

    añadida funcionalidad de palabras clave

--- Mercafour/src/java/mercafour/dao/PalabraClaveFacade.java
+++ Mercafour/src/java/mercafour/dao/PalabraClaveFacade.java
@@ -5,9 +5,11 @@
  */
 package mercafour.dao;
 
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import mercafour.entity.PalabraClave;
 
 /**
@@ -27,6 +29,20 @@ public class PalabraClaveFacade extends AbstractFacade<PalabraClave> {
 
     public PalabraClaveFacade() {
         super(PalabraClave.class);
+    }
+    
+    public PalabraClave findByWord(String s){
+        String palabra = s.toUpperCase();
+        PalabraClave p = null;
+        Query q;
+        
+        q = this.getEntityManager().createQuery("SELECT p FROM PalabraClave p WHERE UPPER(p.palabra) LIKE = :palabra");
+        q.setParameter("palabra", palabra);
+        
+        if(q.getResultList()!= null && !q.getResultList().isEmpty()){
+            p = (PalabraClave) q.getResultList().get(0);
+        }
+        return p;
     }
     
 }
